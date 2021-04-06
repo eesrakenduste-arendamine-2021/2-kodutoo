b98712e0ebe181ef62b5963affe6f0a2496b7930
smtamme <stellamariitamme@gmail.com>
2021-04-06 21:13:30 +0300

    mõned uuendused

--- mang.js
+++ mang.js
@@ -1,15 +1,23 @@
 var cookies = 0;
 var clicks = 0;
+var grandmas = 0;
+var farms = 0;
+var mines = 0;
+var grandmaPrice = 5;
+var farmPrice = 50;
+var minePrice = 200;
+let cookieDiv = document.getElementById('cookies');
 
 $(document).ready(function(){
 
 
     $("#cookie").on('click', function(){
         clicks+=1;
-        let cookiesNr = 1000;
         document.getElementById('clicks').innerHTML = "Klikke tehtud: " + clicks;
+        cookies+=1;
 
-        if(clicks<=10){
+
+        /* if(cookies<=10){
             cookies+=1;
         }else if(cookies==10){
             alert("Palju õnne! \nVanaema aitab küpsetada!")
@@ -28,8 +36,51 @@ $(document).ready(function(){
             cookies+=50;
         }else if(cookies==500){
             alert("Kliki kuni jaksad!")
-        }
-        document.getElementById('cookies').innerHTML = "Küpsiseid: " + cookies;
+        } */
+        cookieDiv.innerHTML = "Küpsiseid: " + cookies;
     })
-     
+    
+    $("#grandma").on('click', function(){
+        if(cookies<grandmaPrice){
+            alert("Sul on liiga vähe küpsiseid vanaema jaoks")
+        }else{
+            cookies = cookies-grandmaPrice;
+            cookieDiv.innerHTML = "Küpsiseid: " + cookies;
+            grandmas+=1;
+            document.getElementById('grandmaNr').innerHTML = grandmas;
+            grandmaPrice = Math.round(grandmaPrice*1.1);
+            document.getElementById('grandmaPrice').innerHTML = grandmaPrice;
+        }
+    })
+    $("#farm").on('click', function(){
+        if(cookies<farmPrice){
+            alert("Sul on liiga vähe küpsiseid farmi jaoks")
+        }else{
+            cookies = cookies-farmPrice;
+            cookieDiv.innerHTML = "Küpsiseid: " + cookies;
+            farms+=1;
+            document.getElementById('farmNr').innerHTML = farms;
+            farmPrice = Math.round(farmPrice*1.1);
+            document.getElementById('farmPrice').innerHTML = farmPrice;
+        }
+    })
+    $("#mine").on('click', function(){
+        if(cookies<minePrice){
+            alert("Sul on liiga vähe küpsiseid kaevanduse jaoks")
+        }else{
+            cookies = cookies-minePrice;
+            cookieDiv.innerHTML = "Küpsiseid: " + cookies;
+            mines+=1;
+            document.getElementById('mineNr').innerHTML = mines;
+            minePrice = Math.round(minePrice*1.1);
+            document.getElementById('minePrice').innerHTML = minePrice;
+        }
+    })
 });
+updateCookies();
+setInterval(updateCookies, 1000);
+function updateCookies(){
+    cookies = cookies+grandmas+farms*10+mines*50;
+    cookieDiv.innerHTML = "Küpsiseid: " + cookies;
+
+}
